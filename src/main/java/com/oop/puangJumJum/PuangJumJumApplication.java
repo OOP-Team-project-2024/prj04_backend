12ee77af69036e9d1105027397791d42e60cf6d1
Handoni <sang1234yun@naver.com>
2024-11-27 00:59:50 +0900

    backend init

---
+++ src/main/java/com/oop/puangJumJum/PuangJumJumApplication.java
@@ -0,0 +1,13 @@
+package com.oop.puangJumJum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PuangJumJumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PuangJumJumApplication.class, args);
+	}
+
+}
