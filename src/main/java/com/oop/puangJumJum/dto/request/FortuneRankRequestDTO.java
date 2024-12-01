e8d0567d3e7b9cbdbd2450a57e87447dd3325172
eunsu02 <regulus_leo08@naver.com>
2024-12-01 22:55:50 +0900

    [feat] 순위 조회 API DTO

---
+++ src/main/java/com/oop/puangJumJum/dto/request/FortuneRankRequestDTO.java
@@ -0,0 +1,10 @@
+package com.oop.puangJumJum.dto.request;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class FortuneRankRequestDTO {
+    private String studentNum;
+}
